c4d23ea6bf490acea4d2d4507e986af55543fa27
James Crace <33646936+jamescrace@users.noreply.github.com>
2024-10-30 16:47:46 -0400

    fixed the environment variables

--- netlify/functions/api-proxy.js
+++ netlify/functions/api-proxy.js
@@ -1,14 +1,7 @@
 export const handler = async (event, context) => {
 
-    //  log function start
-    console.log('Function started', {
-        method: event.httpMethod,
-        path: event.queryStringParameters?.path
-    })
-
-
-    const API_KEY = Netlify.env.get("VITE_API_KEY");
-    const API_BASE_URL = Netlify.env.get("VITE_API_BASE_URL");
+    const API_KEY = process.env.get("VITE_API_KEY");
+    const API_BASE_URL = process.env.get("VITE_API_BASE_URL");
 
     try {
         const path = event.queryStringParameters.path;
@@ -28,15 +21,9 @@ export const handler = async (event, context) => {
         if (event.body) {
             options.body = event.body;
         }
-        // log attempt
-        console.log('Attempting to fetch:', API_BASE_URL + path);
 
         const response = await fetch(API_BASE_URL + path, options);
 
-        // log response
-        console.log('Response received:', {
-            status: response.status
-        });
         const data = await response.json();
 
         return {
@@ -47,7 +34,6 @@ export const handler = async (event, context) => {
     } catch (error) {
         console.error('failure:', {
             error: error.message,
-            stack: error.stack
         });
 
         return {
